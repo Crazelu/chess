07c2925a88def431e8417b42da07c6175117a654
Crazelu <eberelucky64@gmail.com>
2021-07-17 12:38:02 +0100

    Creates flutter project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChessApp());
+}
+
+class ChessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chess',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
